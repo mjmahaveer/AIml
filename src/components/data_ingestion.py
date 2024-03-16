9a5e908d3255bb1b54b778e401aa77b24d122d43
mjmahaveer <mjmahaveerk@gmail.com>
2024-03-16 17:10:43 +0530

    Data Trnasformation added

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,15 +1,18 @@
 import os
 import sys
-# print(sys.path)
-# adding Folder_2 to the system path
+
 sys.path.insert(0, os.getcwd())
-# print (os.getcwd())
+
 from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
+
 
 @dataclass
 class DataIngestionConfig:
@@ -51,7 +54,10 @@ class DataIngestion:
         
 if __name__=="__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
+
 
 
 
