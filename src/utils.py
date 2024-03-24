d79ebcedb6b0886dd004ffd44087774a2590cf2f
mjmahaveer <mjmahaveerk@gmail.com>
2024-03-24 20:12:50 +0530

    Prediction Pipeline

--- src/utils.py
+++ src/utils.py
@@ -22,7 +22,6 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-    
 def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     try:
         report = {}
@@ -53,4 +52,12 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
 
     except Exception as e:
         raise CustomException(e, sys)
- 
+
+def load_object(file_path):
+    try:
+        print(file_path)
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
